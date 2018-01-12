a12e77823b7feddb14900799344901a0b59828b0
Eynaliyev <REynaliyev@poland.luxoft.com>
2018-01-12 18:25:24 +0100

    add and configure ESlint, eslint-watch

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
 
-console.log(`I would like  ${courseValue} apples`);
+console.log(`I would like  ${courseValue} apples`);//eslint-disable-line no-console
